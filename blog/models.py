5516543c57d5997486fcb7092a9de8e4c17e9849
eraysurmeli <surmelimehmeteray@gmail.com>
2021-02-14 20:25:45 +0300

    category modeli olusturuldu

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from autoslug import AutoSlugField
+
 
 # Create your models here.
+class category(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    slug = AutoSlugField(populate_from='name', unique=True)
+
+    class Meta:
+        db_table = 'category'
+    def __str__(self):
+        return self.name
