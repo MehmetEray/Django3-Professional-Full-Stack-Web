9bbf208d5b56e0dc6b7c628f489d979d8545b0fb
eraysurmeli <surmelimehmeteray@gmail.com>
2021-02-14 21:12:25 +0300

    comment olusturuldu

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,39 @@
 from django.contrib import admin
-from blog.models import category
+from blog.models import category, article, comment
+
+
 # Register your models here.
-admin.site.register(category)
+class categoryAdmin(admin.ModelAdmin):
+    search_fields = (
+        'name',
+    )
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(category, categoryAdmin)
+
+
+class articlesAdmin(admin.ModelAdmin):
+    search_fields = (
+        'header', 'content',
+    )
+    list_display = (
+        'header', 'content', 'existed_date', 'edited_date',
+    )
+
+
+admin.site.register(article, articlesAdmin)
+
+
+class commentAdmin(admin.ModelAdmin):
+    search_fields = (
+        'author','article','comment',
+    )
+    list_display = (
+        'author','article','comment',
+    )
+
+
+admin.site.register(comment, commentAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from autoslug import AutoSlugField
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
@@ -18,7 +19,7 @@ class category(models.Model):
 class article(models.Model):
     picture = models.ImageField(upload_to='article_pictures')
     header = models.CharField(max_length=50)
-    content = models.TextField()
+    content = RichTextField()
     existed_date = models.DateTimeField(auto_now_add=True)
     edited_date = models.DateTimeField(auto_now=True)
     slug = AutoSlugField(populate_from='header', unique=True)
@@ -27,3 +28,20 @@ class article(models.Model):
 
     class Meta:
         db_table = 'Article'
+
+    def __str__(self):
+        return self.header
+
+
+class comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comment')
+    article = models.ForeignKey(article, on_delete=models.CASCADE, related_name='comments')
+    comment = models.TextField()
+    existed_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Comment'
+
+    def __str__(self):
+        return self.author
